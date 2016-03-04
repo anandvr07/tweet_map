46b950ecd3f7edec946ffa0756e0e1bb189ce751
Krishnan Kalyanasundar <Krish@Krishnans-MacBook-Pro.local>
2016-03-04 04:36:04 -0500

    insert and select

--- mysql_python.py
+++ mysql_python.py
@@ -12,12 +12,31 @@ import mysql.connector
 
 db = mysql.connector.connect(host="localhost",user="root",password="",database="twittmapy")
 cursor = db.cursor()
-sql_query = "INSERT INTO twitterdata values(1,1.1,-2.6,'hello')"
 
+sql_query = "INSERT INTO twitterdata values(4,1.1,-2.6,'hello')"
 try:
    cursor.execute(sql_query)
    db.commit()
 except:
    db.rollback()
+
+
+sql_query = "SELECT * FROM twitterdata"
+try:
+   cursor.execute(sql_query)
+   
+   results = cursor.fetchall()
+   for row in results:
+      twitt_id = row[0]
+      latitude = row[1]
+      longitude = row[2]
+      search_key = row[3]
+      
+      res_dict = dict(twitt_id=twitt_id, latitude=latitude, longitude=longitude, search_key=search_key)
+
+      print res_dict
+except:
+   print "Error: unable to fecth data"
+
 
 db.close()
